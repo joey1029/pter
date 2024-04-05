9cff80cd4144c697790cfcfcaff8bbb69c3b23ae
joey <641216440@qq.com>
2024-04-06 02:38:31 +0800

    提交

--- src/main/java/com/joey/task/conf/QuartzConfig.java
+++ src/main/java/com/joey/task/conf/QuartzConfig.java
@@ -1,6 +1,7 @@
 package com.joey.task.conf;
 
 import com.joey.task.task.PterJob;
+import com.joey.task.task.SHTJob;
 import org.quartz.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -12,15 +13,26 @@ public class QuartzConfig {
     @Value("${quartz.cron}")
     private String cron; // corn表达式
 
+    @Value("${quartz.shtcron}")
+    private String shtcron;
+
     @Bean
-    public JobDetail dynamicsDefendDolbyTorrent() {
+    public JobDetail pterJobDetail() {
         return JobBuilder.newJob(PterJob.class).withIdentity("PterJob").storeDurably().build();
+    }
+
+    @Bean
+    public JobDetail shtJobDetail() {
+        return JobBuilder.newJob(SHTJob.class).withIdentity("SHTJob").storeDurably().build();
     }
 
     @Bean
     public Trigger restartTrigger() {
         CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(cron);
-        return TriggerBuilder.newTrigger().forJob(dynamicsDefendDolbyTorrent()).withIdentity("PterJob").withSchedule(scheduleBuilder).build();
+        CronScheduleBuilder shtScheduleBuilder = CronScheduleBuilder.cronSchedule(shtcron);
+        return TriggerBuilder.newTrigger()
+                .forJob(pterJobDetail()).withIdentity("PterJob").withSchedule(scheduleBuilder)
+                .forJob(shtJobDetail()).withIdentity("SHTJob").withSchedule(shtScheduleBuilder).build();
     }
 
 }
